64857ae58574c4a43c127606b8c6121ecd189770
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2023-02-11 15:21:52 +0100

    use LoggerInterface service

--- src/Controller/SongsApiController.php
+++ src/Controller/SongsApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,13 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class SongsApiController extends AbstractController
 {
    #[Route('/api/songs/{id<\d+>}',methods:['GET'])]
-    public function getSong(int $id): Response
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
         $song = [
             'id' => $id,
             'song' => 'Gangsta\'s Paradise',
             'artist' => 'Coolio'
         ];
+        $logger->info('Returning API response {song}',[
+            'song'=>$id
+        ]);
         // shortcut 
         //   return $this->json($song);
         return new JsonResponse($song);
