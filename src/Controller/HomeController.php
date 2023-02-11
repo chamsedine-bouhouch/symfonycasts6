948c7e0e88947e3b7d0640c16f8b92317fe0ae8c
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2023-02-11 07:36:41 +0100

    twig-hands-on

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,28 +2,37 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
     #[Route('/')]
     public function home(): Response
     {
-        return new Response('title: done');
+        $skills = [
+            ['name' => 'php', 'version' => 8.2],
+            ['name' => 'laravel', 'version' => 9.2],
+            ['name' => 'symfony', 'version' => 6],
+        ];
+        return $this->render('home.html.twig', [
+            'title' => 'Home',
+            'skills' => $skills
+        ]);
     }
 
     #[Route('/users/{user}')]
     #wildcard params call in the method are optional
     #params order is not important
-    public function users(String $user= null): Response
+    public function users(String $user = null): Response
     {
         if ($user) {
-            $title=str_replace('-',' ',$user);
+            $title = str_replace('-', ' ', $user);
         } else {
-            $title='all users';
+            $title = 'all users';
         }
-        
+
         return new Response("Welcome $title");
     }
 }
